9f3e14caa81b39ca4db4088b5b4c79597d10c5f8
angela1393aa <angela1393aa@gmail.com>
2022-09-23 14:23:30 +0800

    咖啡豆商品

--- action/order.js
+++ action/order.js
@@ -1,4 +1,4 @@
-const { COFFEE_ORDERED } = require("./types");
+const { COFFEE_ORDERED, COFFEEBEAN_ORDERED } = require("./types");
 
 const orderCoffee = (qty, income) => {
   return {
@@ -9,7 +9,17 @@ const orderCoffee = (qty, income) => {
     },
   };
 };
-
+// 購買咖啡豆
+const orderCoffeeBean = (qty, income) => {
+  return {
+    type: COFFEEBEAN_ORDERED,
+    payload: {
+      qty: qty, //數量
+      income: income, //金錢
+    },
+  };
+};
 module.exports = {
   orderCoffee,
+  orderCoffeeBean,
 };
--- action/restock.js
+++ action/restock.js
@@ -1,4 +1,4 @@
-const { COFFEE_RESTOCKED } = require("./types");
+const { COFFEE_RESTOCKED, COFFEEBEAN_RESTOCKED } = require("./types");
 
 // 數量要相加，錢花掉了要相減
 const restockCoffee = (qty, pay) => {
@@ -10,7 +10,17 @@ const restockCoffee = (qty, pay) => {
     },
   };
 };
-
+// 補咖啡豆商品的 function
+const restockCoffeeBean = (qty, pay) => {
+  return {
+    type: COFFEEBEAN_RESTOCKED,
+    payload: {
+      qty: qty,
+      pay: pay,
+    },
+  };
+};
 module.exports = {
   restockCoffee,
+  restockCoffeeBean,
 };
