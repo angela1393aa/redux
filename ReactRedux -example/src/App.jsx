e82796974981229cb97fa88d148f011a688fc6a1
angela1393aa <angela1393aa@gmail.com>
2024-04-27 10:04:24 +0800

    蛋糕購買進貨

--- ReactRedux -example/src/App.jsx
+++ ReactRedux -example/src/App.jsx
@@ -1,11 +1,14 @@
 import CoffeeBlock from "./components/CoffeeBlock"; //咖啡
 import CoffeeBeanBlock from "./components/coffeeBeanBlock"; //咖啡豆
+import CakeBlock from "./components/CakeBlock";
+
 function App() {
   return (
     <div className="container">
       <h1>Restaurant Record</h1>
       <CoffeeBlock />
       <CoffeeBeanBlock />
+      <CakeBlock />
     </div>
   );
 }
--- ReactRedux -example/src/features/store.js
+++ ReactRedux -example/src/features/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import coffeeSlice from "./slices/coffeeSlice";
 import coffeeBeanSlice from "./slices/coffeeBeanSlice";
+import cakeSlice from "./slices/cakeSlice";
 
 const store = configureStore({
   reducer: {
     coffee: coffeeSlice, //咖啡
     coffeeBean: coffeeBeanSlice, //咖啡豆
+    cake: cakeSlice, //蛋糕
   },
 });
 
